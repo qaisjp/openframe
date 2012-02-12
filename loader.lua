91ec10a65a109fca419685a9c4ea01f0cb2422da
noddy.mta@gmail.com <noddy.mta@gmail.com@192825e4-2479-2742-b507-71713afabd94>
2012-02-12 10:20:20 +0000

    [WIKI] Updated basic wiki Also updated loader.lua to work with the wiki.
    git-svn-id: https://openframe.googlecode.com/svn/trunk@3 192825e4-2479-2742-b507-71713afabd94

--- loader.lua
+++ loader.lua
@@ -1,9 +1,13 @@
-local script = {}
+if getResourceName(getThisResource()):lower == "openframe" then
+	local script = {}
 
-function push(str)
-	table.insert(script, str)
-end
+	function push(str)
+		table.insert(script, str)
+	end
 
-function load()
-	return script
+	function load()
+		return script
+	end
+else
+	loadstring(exports.openframe:load())()
 end
